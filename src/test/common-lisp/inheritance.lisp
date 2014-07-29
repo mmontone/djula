9726cd0abfe5ff5148cf4f2c16e803ae1336c659
mmontone <marianomontone@gmail.com>
2014-07-29 20:26:38 -0300

    Added (failing) 'super' tag test

--- src/test/common-lisp/inheritance.lisp
+++ src/test/common-lisp/inheritance.lisp
@@ -8,6 +8,7 @@
 (defparameter +t1+ (djula:compile-template* "t1.djula"))
 (defparameter +t2+ (djula:compile-template* "t2.djula"))
 (defparameter +t3+ (djula:compile-template* "t3.djula"))
+(defparameter +t4+ (djula:compile-template* "t4.djula"))
 
 (test simple-block-test
   (let ((output (djula:render-template* +t1+ nil)))
@@ -30,3 +31,16 @@
 			     (member char (list #\  #\Newline)))
 			   output)
 		"beforeFooafter"))))
+
+(test extends-error-test
+  (signals djula::template-error
+    (let ((djula:*catch-template-errors-p* nil))
+      (djula::compile-string "{% extends \"foo.djula\" %}"))))
+
+;; This test fails, needs a fix
+(test simple-super-test
+  (let ((output (djula:render-template* +t4+ nil)))
+    (is (equalp (remove-if (lambda (char)
+			     (member char (list #\  #\Newline)))
+			   output)
+		"beforeHelloByeafter"))))
