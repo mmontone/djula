32564146b450127fca6b6c388bfc1aacc51183ed
mmontone <marianomontone@gmail.com>
2014-07-30 17:52:54 -0300

    Filters tests

--- test/filters.lisp
+++ test/filters.lisp
@@ -12,8 +12,15 @@
   (is (string= "lower"    (filter :lower "LOWER")))
   (is (string= "short..." (filter :truncatechars "short message" 5)))
   (is (string= "UPPER"    (filter :upper "upper")))
-  (is (=        6         (filter :length "length"))))
+  (is (=        6         (filter :length "length")))
+  (is (string= "&lt;asdf&gt;" (filter :force_escape "<asdf>")))
+  (is (string= "asdf<br />asdf" (filter :linebreaksbr "asdf
+asdf")))
+  (is (string= "LALA" (filter :lisp "lala" "string-upcase")))
+  (is (string= (filter :urlencode "http://www.google.com")
+	       "http%3A%2F%2Fwww.google.com")))
 
 (test apply-filters
   (is (string= "SHORT..."
                (djula::apply-filters "short message" '((:truncatechars 5) (:upper))))))
+
