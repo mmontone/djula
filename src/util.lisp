9662125468b6b575d1aa217b5f1e20fefd568ec4
Javier Olaechea <pirata@gmail.com>
2015-09-07 17:47:42 -0500

    (util.lisp): remove split function
      It was not used throughout the code.

--- src/util.lisp
+++ src/util.lisp
@@ -58,17 +58,3 @@ Similar to Python's str.join"
      (dolist (item list result)
        (setf result (concatenate 'string result separator item))))
    (length separator)))
-
-(defun split (chars str &optional (lst nil) (accm ""))
-  (cond
-    ((= (length str) 0) (reverse (cons accm lst)))
-    (t
-     (let ((c (char str 0)))
-       (if (member c chars)
-	   (split chars (subseq str 1) (cons accm lst) "")
-	   (split chars (subseq str 1) 
-		  lst 
-		  (concatenate 'string
-			       accm
-			       (string c))))
-       ))))
