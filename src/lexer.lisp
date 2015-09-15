857dd82dab336413595d1620ad245b9ffd12b41a
Javier Olaechea <pirata@gmail.com>
2015-09-15 18:29:57 -0500

    (src/lexer.lisp) refactor split-template-string
      Rewrite split-template-string to an iterative version and split the
      functionality across smaller functions

--- src/lexer.lisp
+++ src/lexer.lisp
@@ -16,7 +16,19 @@ Although not a lexer token, the keyword :not-special is used to signify that the
 - :not-special: The previous { is converted into the token (:string "{")
 
 |#
+
+(defun token-type (char)
+  "Return the token-type for CHAR."
+  (case char
+    (#\# :comment)
+    (#\{ :unparsed-variable)
+    (#\_ :unparsed-translation-variable)
+    (#\% :unparsed-tag)
+    (#\$ :verbatim)
+    (otherwise :not-special)))
+
 (defun get-closing-delimiter (type)
+  "Return the string that closes the corresponding token TYPE."
   (ecase type
     (:comment "#}")
     (:unparsed-variable "}}")
@@ -24,36 +36,40 @@ Although not a lexer token, the keyword :not-special is used to signify that the
     (:unparsed-tag "%}")
     (:verbatim "$}")))
 
-(defun split-template-string (string start)
-  (let (({ (position #\{ string :start start :test 'char=)))
-    (if (null {)
-	(if (< start (length string))
-	    (list `(:string ,(subseq string start))))
-	(if (> { start)
-	    (cons (list :string (subseq string start {))
-		  (split-template-string string {))
-	    (let* ((next (char string (1+ {)))
-		   (type (case next
-			   (#\# :comment)
-			   (#\{ :unparsed-variable)
-			   (#\_ :unparsed-translation-variable)
-			   (#\% :unparsed-tag)
-			   (#\$ :verbatim)
-			   (otherwise :not-special))))
-	      (ecase type
-		((:comment 
-		  :unparsed-variable 
-		  :unparsed-translation-variable 
-		  :unparsed-tag
-		  :verbatim)
-		 (let ((end (search (get-closing-delimiter type)
-				    string
-				    :start2 (1+ {))))
-		   (if (null end)
-		       (list (list :string (subseq string start)))
-		       (cons (list type (subseq string (+ 2 {) end))
-			     (split-template-string string (+ 2 end))))))
-		(:not-special (cons `(:string "{") (split-template-string string (1+ start))))))))))
+(defun next-tag (string start)
+  "Return the position of the start of next tag in STRING starting from START."
+  (position #\{ string :start start :test 'char=))
+
+(defun parse-tag (string current-position)
+  "Return the lexer token and the index where the tag ended."
+  (let ((type (token-type (char string (1+ current-position)))))
+    (ecase type
+      ((:comment
+        :unparsed-variable
+        :unparsed-translation-variable
+        :unparsed-tag
+        :verbatim) (let ((end (search (get-closing-delimiter type) string :start2 (1+ current-position))))
+                     (if (null end)
+                         (values `(:string ,(subseq string current-position)) (length string))
+                         (values (list type (subseq string (+ 2 current-position) end)) (+ 2 end))))) ;; The 2 is the hard-coded length of the closing delimiters
+      (:not-special (values '(:string "{")
+                            (1+ current-position))))))
+
+(defun split-template-string (template current-position)
+  "Transform the TEMPLATE into a list of lexer tokens "
+  (let (results)
+    (loop
+      :for { := (next-tag template current-position)
+      :until (null {)
+      :do
+         (when (> { current-position)
+           (push `(:string ,(subseq template current-position {)) results))
+         (multiple-value-bind (token next-position) (parse-tag template {)
+           (push token results)
+           (setf current-position next-position)))
+    (when (< current-position (length template))
+      (push `(:string ,(subseq template current-position)) results))
+    (nreverse results)))
 
 (def-token-compiler :verbatim (string)
   (lambda (stream)
