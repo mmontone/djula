fcc2f80302e1f852d402d9a05ad66c5c91794a0c
Mariano Montone <marianomontone@gmail.com>
2020-10-05 09:36:58 -0300

    template-error as simple-error
    Error arguments are inspectionable from debugger like this.

--- src/conditions.lisp
+++ src/conditions.lisp
@@ -1,13 +1,7 @@
 (in-package #:djula)
 
-(define-condition template-error (error)
-  ((message
-    :reader template-error-message
-    :initarg :message
-    :initform (error "Provide the error message")))
-  (:report
-   (lambda (e stream)
-     (princ (template-error-message e) stream))))
+(define-condition template-error (simple-error)
+  ())
 
 (defun template-error-string (fmt &rest args)
   (format nil "{# Error: ~? #}" fmt args))
@@ -21,18 +15,14 @@
 
 (defun template-error (msg &rest args)
   (error 'template-error
-         :message (if args
-                      (apply #'template-error-string msg args)
-                      msg)))
+         :format-control msg
+         :format-arguments args))
 
 (defun template-error* (error msg &rest args)
   (if *verbose-errors-p*
       (error 'template-error
-             :message (format nil "~A: ~A"
-                              (if args
-                                  (apply #'template-error-string args)
-                                  msg)
-                              error))
+             :format-control "~A: ~A"
+             :format-arguments (list* error args))
       (apply #'template-error msg args)))
 
 (defmacro with-template-error (recovery-form &body body)
